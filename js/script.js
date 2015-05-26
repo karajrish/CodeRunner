31247e6811f2dc1c0d9d7fe86a0457c5e9d9796f
karajrish <rishabh.karajgi@gmail.com>
2015-05-27 01:08:22 +0400

    pushed files

---
+++ js/script.js
@@ -0,0 +1,32 @@
+
+    var windowHeight = $(window).height();
+    //browser window height = 639px
+    var menuBarHeight = $("#topbar").height();
+    var codeContainerHeight = windowHeight - menuBarHeight;
+    $(".codeContainer").height(codeContainerHeight + "px");
+    $(".toggle").click(function(){
+        $(this).toggleClass("selected");
+        var activeDiv = $(this).html();
+        $("#" + activeDiv + "Container").toggle();
+        var showingDivs = $(".codeContainer").filter(function() {
+                                                     
+            return($(this).css("display") != "none");
+        }).length;
+        var width = 100/showingDivs;
+        $(".codeContainer").css("width",width+"%");
+    });
+    
+ $("#RunButton").click(function(){
+                          
+     $("iframe").contents().find("html").html('<style>' + $("#CSSCode").val() + '</style>' + $("#HTMLCode").val());     
+     
+     document.getElementById("resultFrame").contentWindow.eval($("#JSCode").val());
+    });
+
+    $("#clearButton").click(function(){
+    
+        $("HTMLCode").val(" ");
+        $("CSSCode").val(" ");
+        $("JSCode").val(" ");
+    });
+   
